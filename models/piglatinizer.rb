555d376871ea3c942071bebe302edae2685a47c3
Austin Smith <trunk9slamchest@gmail.com>
2019-10-09 16:59:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,26 @@
 class PigLatinizer
 
+	def do_function(input_str)
+		new_phrase = ''
+		vowels = "aAeEiIoOuU"
+		split_phrase = user_phrase.split(" ")
 
+		split_phrase.each { |word|
+			if vowels.include?(word[0])
+				new_phrase += word + "way "
+			else
+				word.each_char.with_index do |char, i|
+					if vowels.include?(char)
+						new_phrase += word[i..-1] + word[0...i] + "ay "
+						break
+					end
+				end
+			end
+		}
+
+new_phrase
+
+	end
 
 
 
