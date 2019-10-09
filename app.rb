faeed89ba79249e188dc20be892454f313e11c37
Austin Smith <trunk9slamchest@gmail.com>
2019-10-09 16:30:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+
+		erb :user_input
+	end
+
+	post '/piglatinize' do
+		@pig_latin = Piglatinize.new(params[:user_phrase])
+
+
+		erb :piglatinize
+	end
 end
