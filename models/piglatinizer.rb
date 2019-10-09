92d52ebe1e1dc9e3e5e3d459840f2e18d64b12cd
Austin Smith <trunk9slamchest@gmail.com>
2019-10-09 17:10:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,23 +2,27 @@ class PigLatinizer
 
 	def piglatinize(input_str)
 		new_phrase = ''
+		new_arr = []
 		vowels = "aAeEiIoOuU"
 		split_phrase = input_str.split(" ")
 
 		split_phrase.each { |word|
 			if vowels.include?(word[0])
-				new_phrase += word + "way" + " "
+				# new_phrase += word + "way"
+				new_arr << word + "way"
 			else
 				word.each_char.with_index do |char, i|
 					if vowels.include?(char)
-						new_phrase += word[i..-1] + word[0...i] + "ay" + " "
+						# new_phrase += word[i..-1] + word[0...i] + "ay"
+						new_arr << word[i..-1] + word[0...i] + "ay"
 						break
 					end
 				end
 			end
 		}
 
-new_phrase
+	new_phrase = new_arr.join(" ")
+	new_phrase
 
 	end
 
