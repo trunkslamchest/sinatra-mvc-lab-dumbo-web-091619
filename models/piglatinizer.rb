143f84cc3a589e44b49b3f292d0667d8c2bec9f2
Austin Smith <trunk9slamchest@gmail.com>
2019-10-09 16:29:37 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,33 @@
+class Piglatinize
+
+	attr_reader :user_phrase
+	# attr_writer
+	# attr_accessor
+
+	def initialize(user_phrase)
+	@user_phrase = user_phrase
+	end
+
+	def do_function
+		new_phrase = ''
+		vowels = "aAeEiIoOuU"
+		split_phrase = user_phrase.split(" ")
+
+		split_phrase.each { |word|
+			if vowels.include?(word[0])
+				new_phrase += word + "way "
+			else
+				word.each_char.with_index do |char, i|
+					if vowels.include?(char)
+						new_phrase += word[i..-1] + word[0...i] + "ay "
+						break
+					end
+				end
+			end
+		}
+
+		new_phrase
+
+	end
+
+end
